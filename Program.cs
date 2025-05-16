735447f8f9c4c93e29a1b01e7251cb7963a92a5f
tungrg <tungrg07@gmail.com>
2025-05-16 17:06:42 +0700

    1st commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Slide2;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+
+public class Program
+{
+    
+    public static void Main(string[] args)
+    {
+
+        var studentList = new List<Student>();
+        studentList.Add(new Student(1, "NguyenVanA", "CNTT"));
+        studentList.Add(new Student(2, "NguyenVanB", "CNTT1"));
+        studentList.Add(new Student(3, "NguyenVanC", "CNTT2"));
+
+        foreach (var student in studentList) {
+            Console.WriteLine($"ID: {student.Id}," +
+                $" Name: {student.Name}, Major: {student.Description}");
+        }
+
+    }
+
+    //public static void demo1()
+    //{
+    //    GetDetailOfParam(3);
+    //    GetDetailOfParam(4.0f);
+    //    GetDetailOfParam("hello");
+    //}
+    //public static void GetDetailOfParam(dynamic para)
+    //{
+    //    para = para * 2;
+    //    Console.WriteLine(para);
+    //    Console.WriteLine(para.GetType());
+    //}
+}
---
+++ Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slide2
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Student(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+    }
+}
